f9c3a2270d3af0ea0a72afe9240db37b128c7f38
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-04-05 16:46:48 -0300

    Implementing repositories in spring-data-jpa style

--- backend/jvm-based/spring-boot-java-app/src/main/java/br/com/vsconsulting/barter/repository/ItemRepository.java
+++ backend/jvm-based/spring-boot-java-app/src/main/java/br/com/vsconsulting/barter/repository/ItemRepository.java
@@ -1,9 +1,7 @@
 package br.com.vsconsulting.barter.repository;
 
 import br.com.vsconsulting.barter.model.Item;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ItemRepository {
-
-  Item save (Item item);
-
+public interface ItemRepository extends PagingAndSortingRepository<Item, Integer> {
 }
