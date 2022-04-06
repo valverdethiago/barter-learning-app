145290fbf4a4fd5eab0b606904332529e843d14c
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-04-06 11:15:04 -0300

    Implementing other business rules and tests for them

--- backend/jvm-based/spring-boot-java-app/src/main/java/br/com/vsconsulting/barter/service/ProposalService.java
+++ backend/jvm-based/spring-boot-java-app/src/main/java/br/com/vsconsulting/barter/service/ProposalService.java
@@ -8,9 +8,16 @@ import br.com.vsconsulting.barter.model.ProposalStatus;
 import br.com.vsconsulting.barter.model.User;
 import br.com.vsconsulting.barter.repository.ItemRepository;
 import br.com.vsconsulting.barter.repository.ProposalRepository;
-import java.util.Optional;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 @Service
 public class ProposalService {
@@ -19,40 +26,74 @@ public class ProposalService {
   private ProposalRepository repository;
   @Autowired
   private ItemRepository itemRepository;
+  @Autowired
+  private MessageSource messageSource;
 
 
-  public void approve(String id) {
+  public void approve(Integer id) {
     Proposal proposal = findById(id);
     validateProposalStatus(proposal);
-    validateItemOwner(proposal);
-    changeItemOwners(proposal);
+    validateItems(proposal);
+    User offeredUser = this.validateAndGetItemsOwner(proposal.getOfferedItems(),
+        "proposal.invalid.status.offered.items.with.different.owners");
+    User targetUser = this.validateAndGetItemsOwner(proposal.getTargetItems(),
+        "proposal.invalid.status.target.items.with.different.owners");
+    changeItemOwners(proposal, offeredUser, targetUser);
     changeProposalStatus(proposal);
   }
 
-  private void validateItemOwner(Proposal proposal) {
-    // TODO implement
+  private void validateItems(Proposal proposal) {
+    if (CollectionUtils.isEmpty(proposal.getOfferedItems())) {
+      throw new InvalidProposalStatusException(messageSource
+          .getMessage("proposal.invalid.status.blank.offered.items", null,
+              Locale.getDefault()));
+    }
+    if (CollectionUtils.isEmpty(proposal.getTargetItems())) {
+      throw new InvalidProposalStatusException(
+          messageSource.getMessage("proposal.invalid.status.blank.target.items", null,
+              Locale.getDefault()));
+    }
+
   }
 
-  private Proposal findById(String id) {
-    Optional<Proposal> proposal = repository.findById(id);
-    if(proposal.isEmpty()) {
-      throw new EntityNotFoundException();
+  private User validateAndGetItemsOwner(Set<Item> items, String exceptionMessageCode) {
+    Map<User, List<Item>> offeredItemsByOwner = items.stream()
+        .collect(Collectors.groupingBy(Item::getOwner));
+    if (offeredItemsByOwner.entrySet().size() > 1) {
+      throw new InvalidProposalStatusException(
+          messageSource.getMessage(exceptionMessageCode,
+              null, Locale.getDefault()));
     }
-    return proposal.get();
+    return offeredItemsByOwner.entrySet().stream().findFirst().orElseThrow(() ->
+        new InvalidProposalStatusException(
+            messageSource.getMessage(exceptionMessageCode,null, Locale.getDefault())))
+        .getKey();
+  }
+
+  private Proposal findById(Integer id) {
+    return repository.findById(id).orElseThrow(
+        () -> new EntityNotFoundException()
+    );
   }
 
   private void changeProposalStatus(Proposal proposal) {
     this.changeProposalStatus(proposal, ProposalStatus.DONE);
   }
 
-  private void changeItemOwners(Proposal proposal) {
-    this.changeOwner(proposal.getOfferedItem(), proposal.getTargetItem().getOwner());
-    this.changeOwner(proposal.getTargetItem(), proposal.getOfferedItem().getOwner());
+  private void changeItemOwners(Proposal proposal, User offeringUser, User targetUser) {
+    proposal.getOfferedItems().stream().forEach( item -> item.setOwner(targetUser));
+    proposal.getTargetItems().stream().forEach( item -> item.setOwner(offeringUser));
+    Set<Item> allItems = Stream.of(proposal.getTargetItems(), proposal.getOfferedItems())
+        .flatMap(x -> x.stream())
+        .collect(Collectors.toSet());
+    itemRepository.saveAll(allItems);
   }
 
   private void validateProposalStatus(Proposal proposal) {
-    if(proposal.getStatus() != ProposalStatus.SUBMITTED) {
-      throw new InvalidProposalStatusException();
+    if (proposal.getStatus() != ProposalStatus.SUBMITTED) {
+      throw new InvalidProposalStatusException(messageSource
+          .getMessage("proposal.invalid.status.other.than.submited", null,
+              Locale.getDefault()));
     }
   }
 
